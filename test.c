35b39a2825b29cab01fe8b9f9dff1284735edeb9
Will Hawkins <whh8b@virginia.edu>
2017-05-30 15:41:42 -0400

    Add a little extra to the test.c file.

--- test.c
+++ test.c
@@ -2,6 +2,12 @@
 int notmain()
 {
 	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	int f = 0;
+	int g = 0;
 	printf("Hello, World.\n");
 	if (a == 0) {
 		return 1;
@@ -9,12 +15,13 @@ int notmain()
 		return 0;
 	}
 }
+
 int main()
 {
 	int a = 0;
 	printf("Hello, World.\n");
 	if (a == 0) {
-		return 1;
+		return notmain();
 	} else {
 		return 0;
 	}
